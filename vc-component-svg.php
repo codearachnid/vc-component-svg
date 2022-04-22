0b9d1cab05f21193e583556e6b6ea9ba2ea3812e
Timothy Wood <codearachnid@gmail.com>
2022-04-22 09:20:16 -0400

    simple cleanup

--- vc-component-svg.php
+++ vc-component-svg.php
@@ -1,18 +1,34 @@
 <?php
 /*
-Plugin Name: SVG for WPBakery
-Description: Custom modules for WPBakery
-Author: codearachnid @codearachnid
-Version: 1.0
-Author URI: https://codearachnid.com/
-*/
+ * @link              https://codearachnid.pro
+ * @since             1.0.0
+ * @package           Vc_Component_Svg
+ *
+ * @wordpress-plugin
+ * Plugin Name:       SVG for WPBakery
+ * Plugin URI:        https://codearachnid.pro
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            codearachnid @codearachnid
+ * Author URI:        https://codearachnid.pro
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       vc-component-svg
+ * Domain Path:       /languages
+ */
 
-if ( ! defined( 'ABSPATH' ) ) { exit; // Exit if accessed directly.
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
 
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'VC_COMPONENT_SVG_VERSION', '1.0.0' );
 
-if ( ! defined( 'ABSPATH' ) ) { exit; // Exit if accessed directly.
-}
 if ( ! class_exists( 'VcComponentSvg' ) ) {
 
 	class VcComponentSvg extends WPBakeryShortCode {
